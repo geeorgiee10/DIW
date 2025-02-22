7d84aa06d0ece3a0495b8e4472d00ed76259662a
Jorge <georgiearclopez@gmail.com>
2025-02-22 13:49:36 +0100

    terminado pagina, a falta de documentacion y gestor de tareas

--- Tema 6/Proyecto Sass/js/index.js
+++ Tema 6/Proyecto Sass/js/index.js
@@ -7,7 +7,6 @@ window.onload = () => {
         navLinks.classList.toggle('active');
     });
 
-    //document.addEventListener("DOMContentLoaded", function() {
         const prevButton = document.getElementById("prev");
         const nextButton = document.getElementById("next");
         const carousel = document.querySelector(".carousel");
@@ -32,7 +31,16 @@ window.onload = () => {
             currentIndex = (currentIndex < items.length - 1) ? currentIndex + 1 : 0;
             updateCarousel();
         });
-    //});
-    
+
+        
+        document.querySelectorAll('.tab').forEach(tab => {
+            tab.addEventListener('click', function() {
+                document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
+                document.querySelectorAll('.tab-content').forEach(tabContent => tabContent.classList.remove('active'));
+        
+                this.classList.add('active');
+                document.getElementById(this.dataset.target).classList.add('active');
+            });
+        });
 
 };
