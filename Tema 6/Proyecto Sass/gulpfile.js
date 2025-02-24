0f867f715a79032da415805d2c935a4c44d2db21
Jorge <georgiearclopez@gmail.com>
2025-02-24 13:28:09 +0100

    Cambios para gulp

--- Tema 6/Proyecto Sass/gulpfile.js
+++ Tema 6/Proyecto Sass/gulpfile.js
@@ -1,20 +1,51 @@
-// indicar que voy a usar gulp
-require("gulp");
-const {series, parallel}=require("gulp");
-//declaracion tareas
+
+const {serie, paralelo, src, dest, swtch } = require ('gulp');
+
+//require('gulp');
+/*
+
 function holamundo(cb){
-    console.log(" Hola mundo");
+    console.log('Hola Mundo');
     cb();
 }
 
-function adiosmundo(cb){
-    console.log("Adios mundo");
+function adios(cb){
+    console.log('Adios Mundo');
     cb();
 }
 
-//publicar la tarea
-exports.holamundo = holamundo;
-exports.adiosmundo = adiosmundo;
-exports.default = holamundo;
-exports.serie = series(adiosmundo, holamundo);
-exports.paralelo = parallel(holamundo, adiosmundo);
+exports.holamundo = holamundo;  //nombre de la tarea fuera, y nonombre de la tarea dentro
+exports.adios = adios; 
+exports.default = holamundo;  //nombre de la tarea fuera, y nonombre de la tarea dentro
+exports.serie = adios,holamundo;
+exports.paralelo = holamundo, adios;
+
+*/
+
+const rename=require('gulp-rename'); // npm install gulp-rename
+//const pleeease=require('gulp-pleeease'); //Obsoleto
+const mincss=require('cssmin');  // npm i cssmin
+const sass = require('gulp-dart-scss'); // npm i gulp-dart-sass
+const sassDoc = require('sassdoc'); // npm i gulp-sass ??
+
+//processhtml para procesas los comentarios
+
+function minimiza_y_renombrar(){
+    return src('./subir/css/style.css')
+        .pipe(pleeease())
+        .pipe(rename({suffix:".min", sxtname:".css"
+    }))
+        .pipe(dest('./subir/css'));
+}
+
+function compilar(){
+    return src('scss/style.scss')
+    .pipe(sass())
+    .pipe(dest('./subir/css'));
+}
+
+function html(){
+    return src('*.html')
+    .pipe(processhtml())
+    .pipe(dest('./subir/'));
+}
